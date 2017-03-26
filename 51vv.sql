6fde38468b9cd651fc1c6cb719eb80b42dce4458
chenhailiang <393276956@qq.com>
2017-03-26 18:54:34 +0800

    .

--- 51vv.sql
+++ 51vv.sql
@@ -1,19 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.1.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2017-03-16 15:57:43
--- 服务器版本： 10.1.10-MariaDB
--- PHP Version: 7.0.2
+-- Generation Time: 2017-03-26 12:52:59
+-- 服务器版本： 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8*/;
+/*!40101 SET NAMES utf8 */;
+
 --
 -- Database: `51vv`
 --
@@ -21,16 +23,78 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `51_admin`
+--
+
+CREATE TABLE IF NOT EXISTS `51_admin` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `userpass` varchar(100) NOT NULL,
+  `salt` varchar(50) NOT NULL,
+  `status` int(2) NOT NULL,
+  `beizhu` varchar(500) NOT NULL,
+  `telephone` varchar(16) NOT NULL,
+  `addtime` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `51_admin`
+--
+
+INSERT INTO `51_admin` (`id`, `username`, `userpass`, `salt`, `status`, `beizhu`, `telephone`, `addtime`) VALUES
+(1, 'admin', 'b23877f42b7630934e969496e6793b54', 'AQfVOeKdUPhckaLF', 1, '系统管理员', '13555555555', '2017-03-17 08:09:46');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `51_bankcard`
+--
+
+CREATE TABLE IF NOT EXISTS `51_bankcard` (
+  `id` int(32) NOT NULL,
+  `bankname` varchar(20) NOT NULL,
+  `cardname` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `51_log`
+--
+
+CREATE TABLE IF NOT EXISTS `51_log` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `addtime` varchar(20) DEFAULT NULL,
+  `record` varchar(500) DEFAULT NULL,
+  `recordcontent` varchar(500) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- 转存表中的数据 `51_log`
+--
+
+INSERT INTO `51_log` (`id`, `addtime`, `record`, `recordcontent`, `username`) VALUES
+(1, '2017-03-26 11:08:10', '后台登录', '用户名：admin；用户密码：admin', ''),
+(2, '2017-03-26 11:13:12', '修改密码', '修改密码为：123qwe', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `51_user`
 --
 
-CREATE TABLE `51_user` (
-  `id` int(4) NOT NULL,
+CREATE TABLE IF NOT EXISTS `51_user` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `ip` varchar(32) NOT NULL,
-  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=34 ;
 
 --
 -- 转存表中的数据 `51_user`
@@ -68,27 +132,35 @@ INSERT INTO `51_user` (`id`, `username`, `password`, `ip`, `addtime`) VALUES
 (29, '4324234', 'hdfgh5sdfsgdfge', '127.0.0.1', '2016-08-07 11:29:55'),
 (30, '43234234324', 'dsdfgsdfbsdf', '127.0.0.1', '2016-08-07 11:30:01'),
 (31, '112344', 'sfasdfs', '127.0.0.1', '2016-12-12 14:10:46'),
-(32, '112344', 'sfasdfs', '127.0.0.1', '2017-03-16 14:48:02');
+(32, '112344', 'sfasdfs', '127.0.0.1', '2017-03-16 14:48:02'),
+(33, '12133444', '123qwe', '127.0.0.1', '2017-03-26 09:24:48');
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- 表的结构 `51_userinfo`
 --
 
---
--- Indexes for table `51_user`
---
-ALTER TABLE `51_user`
-  ADD PRIMARY KEY (`id`);
+CREATE TABLE IF NOT EXISTS `51_userinfo` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `realname` varchar(10) DEFAULT NULL,
+  `usercard` varchar(20) DEFAULT NULL,
+  `bankno` varchar(20) DEFAULT NULL,
+  `bankname` varchar(50) DEFAULT NULL,
+  `cardtype` varchar(10) DEFAULT NULL,
+  `cardno` varchar(20) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `usermobile` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- 在导出的表使用AUTO_INCREMENT
+-- 转存表中的数据 `51_userinfo`
 --
 
---
--- 使用表AUTO_INCREMENT `51_user`
---
-ALTER TABLE `51_user`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+INSERT INTO `51_userinfo` (`id`, `realname`, `usercard`, `bankno`, `bankname`, `cardtype`, `cardno`, `username`, `usermobile`) VALUES
+(1, '的说法', '111111111111111111', NULL, NULL, NULL, NULL, NULL, NULL);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
